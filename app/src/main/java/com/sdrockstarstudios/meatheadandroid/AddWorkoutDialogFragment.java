69b8cd9921e999e9bbd76474ad5f193c946c5b70
sdrockstar <sdebellis462@gmail.com>
2021-06-19 16:30:56 -0700

    Implement WorkoutLog menu and add workout dialog. Wire button to load workout log activity. Change order of screen loading where main menu loads workout log menu.

--- app/src/main/java/com/sdrockstarstudios/meatheadandroid/AddWorkoutDialogFragment.java
+++ app/src/main/java/com/sdrockstarstudios/meatheadandroid/AddWorkoutDialogFragment.java
@@ -3,32 +3,30 @@ package com.sdrockstarstudios.meatheadandroid;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.widget.EditText;
-import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-
-public class AddExerciseDialogFragment extends DialogFragment{
+public class AddWorkoutDialogFragment  extends DialogFragment{
 
     public interface NoticeDialogListener {
         public void onDialogPositiveClick(DialogFragment dialog);
         public void onDialogNegativeClick(DialogFragment dialog);
     }
 
-    NoticeDialogListener listener;
+    AddWorkoutDialogFragment.NoticeDialogListener listener;
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         try {
-            listener = (NoticeDialogListener) context;
+            listener = (AddWorkoutDialogFragment.NoticeDialogListener) context;
         }
         catch (ClassCastException e) {
             throw new ClassCastException(getActivity().toString() + "must implement NoticeListener");
@@ -41,18 +39,18 @@ public class AddExerciseDialogFragment extends DialogFragment{
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
 
-        builder.setView(inflater.inflate(R.layout.dialog_add_exercise, null))
+        builder.setView(inflater.inflate(R.layout.dialog_add_workout, null))
                 .setPositiveButton(R.string.add_dialog_accept, (dialog, id) ->
-                        listener.onDialogPositiveClick(AddExerciseDialogFragment.this))
+                        listener.onDialogPositiveClick(AddWorkoutDialogFragment.this))
                 .setNegativeButton(R.string.cancel, (dialog, which) ->
-                        listener.onDialogNegativeClick(AddExerciseDialogFragment.this));
+                        listener.onDialogNegativeClick(AddWorkoutDialogFragment.this));
 
         AlertDialog dialog = builder.create();
 
         dialog.setOnShowListener(dialogInterface -> {
             dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
-            EditText exerciseNameEditText = dialog.findViewById(R.id.exercise_name_entry);
-            exerciseNameEditText.addTextChangedListener(new TextWatcher() {
+            EditText workoutNameEditText = dialog.findViewById(R.id.workout_name_entry);
+            workoutNameEditText.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
