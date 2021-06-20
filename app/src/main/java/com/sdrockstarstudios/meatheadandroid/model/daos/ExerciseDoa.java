e3abb8c6091ecc86253a4dfcc2e76afe0315dbee
SMDeBellis <SMDeBellis@users.noreply.github.com>
2021-06-20 13:27:44 -0700

    Add deletion methods to exercise Doa. (#11)
    Add tests to test deletion of exercise and all its sets.

--- app/src/main/java/com/sdrockstarstudios/meatheadandroid/model/daos/ExerciseDoa.java
+++ app/src/main/java/com/sdrockstarstudios/meatheadandroid/model/daos/ExerciseDoa.java
@@ -1,6 +1,7 @@
 package com.sdrockstarstudios.meatheadandroid.model.daos;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -12,14 +13,18 @@ import com.sdrockstarstudios.meatheadandroid.model.tables.Exercise;
 import java.util.List;
 
 import io.reactivex.Completable;
+import io.reactivex.Single;
 
 @Dao
 public interface ExerciseDoa {
 
     @Transaction
     @Query("SELECT * FROM exercise")
-    public List<ExerciseAndSets> getExerciseAndSets();
+    public Single<List<ExerciseAndSets>> getExerciseAndSets();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     Completable insert(Exercise exercise);
+
+    @Delete
+    Completable delete(Exercise exercise);
 }
